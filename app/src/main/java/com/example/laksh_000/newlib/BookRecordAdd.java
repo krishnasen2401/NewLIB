4202bc504f2c1bc139681de40667eca08857af21
Lingam mohan Krishna sen <2401krishnasen@gmail.com>
2018-07-27 21:37:20 +0530

    aDD bookhistory and split database helper by extending orginal helper added check null condition update

--- app/src/main/java/com/example/laksh_000/newlib/BookRecordAdd.java
+++ app/src/main/java/com/example/laksh_000/newlib/BookRecordAdd.java
@@ -13,21 +13,33 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class BookRecordAdd extends AppCompatActivity {
+    Date now;
+    String Datenew;
+    BookRecord t;
 public void SubmitRec(View view){
     EditText bookid11=findViewById(R.id.BookidRec);
     EditText studentid11=findViewById(R.id.StudentIdRec);
     String bookid112=bookid11.getText().toString().toUpperCase();
     String studentid112=studentid11.getText().toString().toUpperCase();
-    Date now;
     now= Calendar.getInstance().getTime();
-    String Datenew=new SimpleDateFormat("dd-MM-yyyy").format(now);
+    Datenew=new SimpleDateFormat("dd-MM-yyyy").format(now);
     BookHistory hs=new BookHistory(bookid112,studentid112,Datenew,null);
-    BookRecord t=new BookRecord(this);
     t.addNewRecord(hs,this);
+    bookid11.setText(null);
+    studentid11.setText(null);
+}
+public void ReceviedBk(View v){
+    EditText ReceBkid1=findViewById(R.id.ReceBkid);
+    String ReceBkid112=ReceBkid1.getText().toString().toUpperCase();
+    now=Calendar.getInstance().getTime();
+    Datenew=new SimpleDateFormat("dd-MM-yyyy").format(now);
+    t=new BookRecord(this);
+    t.UpdateRecord(ReceBkid112,Datenew);
 }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_record_add);
+        t=new BookRecord(this);
     }
 }
--- app/src/main/java/com/example/laksh_000/newlib/Helpers/BookRecord.java
+++ app/src/main/java/com/example/laksh_000/newlib/Helpers/BookRecord.java
@@ -27,10 +27,29 @@ public class BookRecord extends MainDatabasehelper {
         values.put(Record_sentdate,recordid.getSentdate());
         values.put(Record_recevicedate,recordid.getRecevieddate());
         db.insert(TABLE_Record,null,values);
-        db.execSQL("update "+TABLE_NAME+" set "+BOOK_Status+"='N' where "+COLUMN_ID+"='"+recordid.getBookid()+"'");
+        db.execSQL("update "+TABLE_NAME+" set "+BOOK_Status+"='N' where "+COLUMN_ID+" like '"+recordid.getBookid()+"'");
         }}
         else {
             Log.i("failed","not created");}
         db.close();
+    }
+    public void UpdateRecord(String bookid,String date){
+        SQLiteDatabase db=this.getWritableDatabase();
+        String query="Select * from "+TABLE_Record+"";
+        /*Cursor cursor = db.rawQuery(query, null);try{
+        if (cursor.moveToFirst()) {
+            do {
+                Log.i("bookid", cursor.getString(cursor.getColumnIndex(Record_bookid)));
+                Log.i("studentid", cursor.getString(cursor.getColumnIndex(Record_studentid)));
+                Log.i("sentdate", cursor.getString(cursor.getColumnIndex(Record_sentdate)));
+                if(cursor.isNull(3))
+                Log.i("recdate1", cursor.getString(3));
+            } while (cursor.moveToNext());
+        }}catch(Exception e){e.printStackTrace();}*/
+
+        db.execSQL("update "+TABLE_NAME+" set "+BOOK_Status+"='Y' where "+COLUMN_ID+"='"+bookid+"'");//updatepresent record table
+        db.execSQL("update "+TABLE_Record+" set "+Record_recevicedate+"='"+date+"' where "+Record_bookid+"='"+bookid+"' and "+Record_recevicedate+" IS NULL");
+        //cursor.close();
+        db.close();//upda1temain book table status
     }
 }
